4f72be063a7ddf78e9db81360b417bc8b15d4a02
brian419 <json10@crimson.ua.edu>
2024-10-02 13:54:21 -0500

    working on login

--- capstone-computing-project/src/app/login-page/page.tsx
+++ capstone-computing-project/src/app/login-page/page.tsx
@@ -1,13 +1,11 @@
-// This will be for login page
-
-// they wanted team picture or lake picture on login page
 "use client";
-
+//9e1b32 9E1B32
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import WaterskiImage from '../img/loginSkiIMG.svg';
+import SkiBamaLogo from '../img/skibamalogo.svg';
 
 export default function LoginPage() {
     const [email, setEmail] = useState('');
@@ -15,10 +13,10 @@ export default function LoginPage() {
     const [fname, setFname] = useState('');
     const [lname, setLname] = useState('');
     const [cwid, setCwid] = useState('');
-    const [gradYear, setGradYear] = useState('Freshman');  // defaulted to freshman. should be removed later, it was just in the database so i copied it over
-    const [memberType, setMemberType] = useState('Athlete');   // default as athlete
+    const [gradYear, setGradYear] = useState('Freshman');
+    const [memberType, setMemberType] = useState('Athlete');
     const [major, setMajor] = useState('');
-    const [isLogin, setIsLogin] = useState(true);  // toggle between login and sign-up
+    const [isLogin, setIsLogin] = useState(true);
     const router = useRouter();
 
     const handleSubmit = async (e) => {
@@ -26,154 +24,143 @@ export default function LoginPage() {
 
         const endpoint = isLogin ? 'http://localhost:4000/auth/login' : 'http://localhost:4000/auth/signup';
 
-        // create the payload for the API call
         const payload = isLogin
-            ? { email, password }  // for login
+            ? { email, password }
             : { email, password, fname, lname, cwid, gradYear, memberType, major };
 
         try {
             const response = await axios.post(endpoint, payload);
             if (isLogin) {
-                localStorage.setItem('token', response.data.token);  // stores token if login is successful
-                console.log('Login successful:', response.data.token);
-                router.push('/protected-pages/protected-home-page'); // protected pages so we can remove certain elements from this page than on homepage, etc (login button can become either Photo they upload for the team to see or their Initials)
+                localStorage.setItem('token', response.data.token);
+                router.push('/protected-pages/protected-home-page');
             } else {
-                console.log('Signup successful:', response.data.message);
-                setIsLogin(true); // switches to login page after successful signup
-        
+                setIsLogin(true);
             }
         } catch (error) {
-            console.error('Error:', error.response?.data?.message || error.message); // debugging
+            console.error('Error:', error.response?.data?.message || error.message);
         }
     };
 
     return (
-        <div className='login-page flex items-center justify-center min-h-screen bg-[#f4f4f9]'>
+        <div className='login-page flex items-center justify-center min-h-screen bg-[#ffffff]'> {/* bg-[#f4f4f9] */}
+            <div className="flex flex-row w-full max-w-6xl">
+                <div className="flex flex-col justify-center items-center w-1/2 pr-8">
+                    {/* Ski Bama Logo */}
+                    <div className="mb-8">
+                        <Image src={SkiBamaLogo} alt="Ski Bama Logo" width={300} height={300} />
+                    </div>
 
-            <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-black'>
-                <h2 className='text-2xl font-bold text-center mb-8 text-black'>{isLogin ? 'Login' : 'Sign Up'}</h2>
-                <div className='flex justify-between mb-4'>
-                    <button
-                        onClick={() => setIsLogin(true)}
-                        className={`w-1/2 mr-1 py-2 rounded-l-full font-bold ${isLogin ? 'bg-[#F96868] text-white' : 'bg-gray-200 text-[#F96868]'}`}
-                    >
-                        Login
-                    </button>
-                    <button
-                        onClick={() => setIsLogin(false)}
-                        className={`w-1/2 ml-1 py-2 rounded-r-full font-bold ${!isLogin ? 'bg-[#F96868] text-white' : 'bg-gray-200 text-[#F96868]'}`}
-                    >
-                        Sign Up
-                    </button>
+                    {/* Login Form */}
+                    <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-black mt-[-40px]'>
+                        <div className='flex justify-between mb-4'>
+                            <button
+                                onClick={() => setIsLogin(true)}
+                                className={`w-1/2 mr-1 py-2 rounded-l-full font-bold ${isLogin ? 'bg-[#9E1B32] text-white' : 'bg-gray-200 text-[#9E1B32]'}`}
+                            >
+                                Login
+                            </button>
+                            <button
+                                onClick={() => setIsLogin(false)}
+                                className={`w-1/2 ml-1 py-2 rounded-r-full font-bold ${!isLogin ? 'bg-[#9E1B32] text-white' : 'bg-gray-200 text-[#9E1B32]'}`}
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+
+                        <form onSubmit={handleSubmit} className='space-y-4'>
+                            <input
+                                type='email'
+                                placeholder='Email Address'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                required
+                            />
+                            <input
+                                type='password'
+                                placeholder='Password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                required
+                            />
+
+                            {!isLogin && (
+                                <>
+                                    <input
+                                        type='text'
+                                        placeholder='First Name'
+                                        value={fname}
+                                        onChange={(e) => setFname(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    />
+                                    <input
+                                        type='text'
+                                        placeholder='Last Name'
+                                        value={lname}
+                                        onChange={(e) => setLname(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    />
+                                    <input
+                                        type='text'
+                                        placeholder='CWID'
+                                        value={cwid}
+                                        onChange={(e) => setCwid(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    />
+                                    <select
+                                        value={gradYear}
+                                        onChange={(e) => setGradYear(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    >
+                                        <option value='Freshman'>Freshman</option>
+                                        <option value='Sophomore'>Sophomore</option>
+                                        <option value='Junior'>Junior</option>
+                                        <option value='Senior'>Senior</option>
+                                    </select>
+                                    <select
+                                        value={memberType}
+                                        onChange={(e) => setMemberType(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    >
+                                        <option value='Athlete'>Athlete</option>
+                                        <option value='Officer'>Officer</option>
+                                    </select>
+                                    <input
+                                        type='text'
+                                        placeholder='Major'
+                                        value={major}
+                                        onChange={(e) => setMajor(e.target.value)}
+                                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
+                                        required
+                                    />
+                                </>
+                            )}
+
+                            <button type='submit' className='w-full bg-[white] text-[black] py-2 rounded-full font-bold'>
+                                {isLogin ? 'Sign In' : 'Sign Up'}
+                            </button>
+                        </form>
+
+                        {isLogin && (
+                            <p className='text-center mt-4'>
+                                <a href='/forgot-password' className='text-[black] hover:underline font-bold'>
+                                    Forgot Password?
+                                </a>
+                            </p>
+                        )}
+                    </div>
                 </div>
 
-                <form onSubmit={handleSubmit} className='space-y-4'>
-                    {/* Email Input */}
-                    <input
-                        type='email'
-                        placeholder='Email Address'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                        required
-                    />
-
-                    {/* Password Input */}
-                    <input
-                        type='password'
-                        placeholder='Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                        required
-                    />
-
-                    {/* following only shows up in Sign Up option in login page */}
-                    {!isLogin && (
-                        <>
-                            {/* First Name Input */}
-                            <input
-                                type='text'
-                                placeholder='First Name'
-                                value={fname}
-                                onChange={(e) => setFname(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            />
-
-                            {/* Last Name Input */}
-                            <input
-                                type='text'
-                                placeholder='Last Name'
-                                value={lname}
-                                onChange={(e) => setLname(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            />
-
-                            {/* CWID Input */}
-                            <input
-                                type='text'
-                                placeholder='CWID'
-                                value={cwid}
-                                onChange={(e) => setCwid(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            />
-
-                            {/* Graduation Year Input */}
-                            <select
-                                value={gradYear}
-                                onChange={(e) => setGradYear(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            >
-                                <option value='Freshman'>Freshman</option>
-                                <option value='Sophomore'>Sophomore</option>
-                                <option value='Junior'>Junior</option>
-                                <option value='Senior'>Senior</option>
-                            </select>
-
-                            {/* Member Type Input */}
-                            <select
-                                value={memberType}
-                                onChange={(e) => setMemberType(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            >
-                                <option value='Athlete'>Athlete</option>
-                                <option value='Officer'>Officer</option>
-                            </select>
-
-                            {/* Major Input */}
-                            <input
-                                type='text'
-                                placeholder='Major'
-                                value={major}
-                                onChange={(e) => setMajor(e.target.value)}
-                                className='w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#49A097]'
-                                required
-                            />
-                        </>
-                    )}
-
-                    {/* Submit Button */}
-                    <button type='submit' className='w-full bg-[#F96868] text-white py-2 rounded-full font-bold'>
-                        {isLogin ? 'Login' : 'Sign Up'}
-                    </button>
-                </form>
-
-                {/* Forgot Password link can be deleted */}
-                {isLogin && (
-                    <p className='text-center mt-4'>
-                        <a href='/forgot-password' className='text-[#49A097] hover:underline'>
-                            Forgot Password?
-                        </a>
-                    </p>
-                )}
-            </div>
-            <div className="flex justify-center mb-12">
-                <Image src={WaterskiImage} alt="Water ski image" width={600} height={600}/>
+                {/* Right side for the team/lake picture */}
+                <div className="w-1/2 flex justify-center items-center">
+                    <Image src={WaterskiImage} alt="Water ski image" width={600} height={600} />
+                </div>
             </div>
         </div>
     );
