f79170904f0e1f5dd09c79ea5455178112c08f48
brian419 <json10@crimson.ua.edu>
2024-10-13 12:57:47 -0500

    Updating navbar to be usable on mobile view. You guys can check with browser, inspect element, and change 'device' by changing 'Dimensions:' at the top, below the website link

--- capstone-computing-project/src/components/Navbar.tsx
+++ capstone-computing-project/src/components/Navbar.tsx
@@ -1,35 +1,82 @@
 "use client";
 
 import Link from 'next/link';
-import HeaderWLAM from './img/headerWLAM.svg';
 import Image from 'next/image';
+import HeaderWLAM from './img/headerWLAM.svg';
 import { useState } from 'react';
 
 export default function Navbar() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false); // for mobile menu toggle
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false); // for about's dropdown toggle
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
 
     return (
         <nav className="bg-[#9E1B32] p-4 shadow-md sticky top-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <Link href="/"> {/* lets the image be clickable and lead back to homepage */}
-                        <Image src={HeaderWLAM} alt="Header WLAM image" width={300} height={300} />
-                    </Link>
+                {/* where legends are made logo */}
+                <Link href="/">
+                    <Image 
+                        src={HeaderWLAM} 
+                        alt="Header WLAM image" 
+                        width={300} 
+                        height={300} 
+                        className="w-24 h-24 md:w-40 md:h-40" // handles different screen sizes
+                    />
+                </Link>
+
+                {/* dropdown for mobile view */}
+                <div className="md:hidden">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none">
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            {isOpen ? (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            ) : (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                />
+                            )}
+                        </svg>
+                    </button>
                 </div>
-                <div className="space-x-10 flex items-center">
+
+                {/* navigation links only for desktop screen sizes */}
+                <div className="hidden md:flex space-x-10 items-center">
                     <Link href="/" className="text-white text-lg hover:text-black hover:underline transition duration-300">
                         Home
                     </Link>
                     <Link href="/set-list-page" className="text-white text-lg hover:text-black hover:underline transition duration-300">
                         Set List
                     </Link>
-                    {/* About Dropdown */}
+
+                    {/* about dropdown only for desktop screen sizes */}
                     <div className='cursor-pointer items-center gap-1 group relative'>
                         <div className='flex flex-row text-white text-lg hover:text-black hover:underline transition duration-300'>
-                            <Link
-                                href='/about-me-page'
-                                className='flex items-center py-2 px-4'
-                            >
+                            <Link href="/about-me-page" className='flex items-center py-2 px-4'>
                                 About 
                                 <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
@@ -38,40 +85,80 @@ export default function Navbar() {
                         </div>
                         <ul className='left-1/2 transform -translate-x-1/2 hidden absolute pt-4 w-40 rounded-lg group-hover:block border-bg-black text-center'>
                             <li className='border-2 hover:bg-[#DE3333] hover:text-white text-black bg-white'>
-                                <Link
-                                    href='/roster-page'
-                                    className='block py-2 px-4'
-                                >
+                                <Link href="/roster-page" className='block py-2 px-4'>
                                     Roster
                                 </Link>
                             </li>
                             <li className='border-2 hover:bg-[#DE3333] hover:text-white text-black bg-white'>
-                                <Link
-                                    href='/club-information-page'
-                                    className='block py-2 px-4'
-                                >
+                                <Link href="/club-information-page" className='block py-2 px-4'>
                                     Club Info
                                 </Link>
                             </li>
                             <li className='border-2 hover:bg-[#DE3333] hover:text-white text-black bg-white'>
-                                <Link
-                                    href='/merch-page'
-                                    className='block py-2 px-4'
-                                >
+                                <Link href="/merch-page" className='block py-2 px-4'>
                                     Merch
                                 </Link>
                             </li>
                         </ul>
                     </div>
+
                     <Link href="/officer-resources-page" className="text-white text-lg hover:text-black hover:underline transition duration-300">
                         Officer Resources
                     </Link>
                 </div>
-                <div>
+
+                {/* login button only for desktop screen sizes */}
+                <div className="hidden md:block">
                     <Link href="/login-page" className="bg-white-500 text-white text-lg py-2 px-4 rounded hover:bg-white hover:text-[#9E1B32] transition duration-300">
                         Login
                     </Link>
                 </div>
+            </div>
+
+            {/* navbar for mobile screens */}
+            <div className={`md:hidden ${isOpen ? 'block' : 'hidden'} mt-4 space-y-4`}>
+                <Link href="/" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                    Home
+                </Link>
+                <Link href="/set-list-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                    Set List
+                </Link>
+
+                {/* about dropdown for mobile sreen sizes */}
+                <div>
+                    <button onClick={toggleDropdown} className="flex justify-between items-center w-full text-white text-lg hover:text-black transition duration-300">
+                        About 
+                        <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+                        </svg>
+                    </button>
+                    {isDropdownOpen && (
+                        <ul className="space-y-2 mt-2 bg-[#D67D7D]">
+                            <li>
+                                <Link href="/roster-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                                    Roster
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/club-information-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                                    Club Info
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/merch-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                                    Merch
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                </div>
+
+                <Link href="/officer-resources-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                    Officer Resources
+                </Link>
+                <Link href="/login-page" className="block text-white text-lg hover:text-black transition duration-300" onClick={closeMenu}>
+                    Login
+                </Link>
             </div>
         </nav>
     );
