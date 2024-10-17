caab88cb6efe1f6d2084b2536e137104cff0a1d4
anastasiaspencer <spenceranastasia7@gmail.com>
2024-10-17 16:14:44 -0500

    Changed  Profile to be a side bar pop up

--- capstone-computing-project/src/app/protected-pages/protected-home-page/page.tsx
+++ capstone-computing-project/src/app/protected-pages/protected-home-page/page.tsx
@@ -1,37 +1,51 @@
-// This will be for home page after loging in]
 "use client"
 import Image from 'next/image';  // Make sure to import Image from 'next/image'
 import ellipseImage from '../../img/DefaultPFP.svg';  // Adjust the path as necessary
 import Link from 'next/link';
-;
+import { useState } from 'react';
+import ProtectedProfilePage from '../protected-profile-page/page'; 
 
 export default function HomeAfterLogin() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to manage sidebar visibility
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
 
     return (
         <div>
             <div>
-        
-
-            <Link href="/protected-pages/protected-profile-page">
-                <Image 
-                    src={ellipseImage} 
-                    alt="Header WLAM image" 
-                    width={80} 
-                    height={80} 
-                    className="h-20 w-20 md:h-28 md:w-28 lg:h-48 lg:w-48 object-contain" // image scales within navbar height and width with different screen sizes
-                />
-            </Link>
+                {/* Button to open the sidebar */}
+                <button onClick={toggleSidebar}>
+                    <Link href="#">
+                        <Image 
+                            src={ellipseImage} 
+                            alt="Header WLAM image" 
+                            width={80} 
+                            height={80} 
+                            className="h-20 w-20 md:h-28 md:w-28 lg:h-48 lg:w-48 object-contain" 
+                        />
+                    </Link>
+                </button>
             </div>
      
-            
+            {/* Sidebar */}
+            {isSidebarOpen && (
+                <div className="fixed right-0 top-0 w-90 h-full bg-white shadow-lg z-50">
+                    <button onClick={toggleSidebar} className="p-2">Close</button>
+                    {/* Render the profile page content here */}
+                    <ProtectedProfilePage />
+                    {/* Add more links or content here */}
+                </div>
+            )}
+
+            {/* Background overlay when sidebar is open */}
+            {isSidebarOpen && <div className="fixed inset-0 bg-black opacity-50 z-40" onClick={toggleSidebar} />}
 
             <main className="flex-grow">
                 {/* Hero Section */}
-                {/* bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white */}
                 <section className="bg-white py-28">
-
                     <div className="container mx-auto text-center flex flex-col justify-center items-center min-h-[70vh]">
-                        {/* Add photo of Skibama team */}
                         <h1 className="text-6xl font-extrabold mb-6 text-[#F96868]">
                             THIS SHOULD ONLY SHOW UP AFTER YOU LOGGED IN
                         </h1>
@@ -68,4 +82,3 @@ export default function HomeAfterLogin() {
         </div>
     );
 }
-
