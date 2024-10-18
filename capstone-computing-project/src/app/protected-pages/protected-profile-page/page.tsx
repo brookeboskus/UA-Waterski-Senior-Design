e1b210ec423d3174d8d035de2bcdc5488c7b345f
anastasiaspencer <spenceranastasia7@gmail.com>
2024-10-17 20:28:54 -0500

    Added Icons for PFP side bar and organized things into place

--- capstone-computing-project/src/app/protected-pages/protected-profile-page/page.tsx
+++ capstone-computing-project/src/app/protected-pages/protected-profile-page/page.tsx
@@ -3,6 +3,14 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import ellipseImage from '../../img/DefaultPFP.svg';
+import FirstNameImage from '../../img/Text (1).svg';
+import LastNameImage from '../../img/Text (2).svg';
+import GradYearImage from '../../img/Text (3).svg';
+import PhoneNumberImage from '../../img/Text (4).svg';
+import EmailImage from '../../img/Text (5).svg';
+import CWIDImage from '../../img/Text (6).svg';
+import MajorImage from '../../img/Text (7).svg';
+import StatusImage from '../../img/Text (8).svg';
 import axios from 'axios';
 
 interface TeamMember {
@@ -50,8 +58,8 @@ export default function ProfilePage() {
         <div className="absolute left-0 top-0 w-full h-[320px] bg-[#9e1b32] z-10"></div> 
 
         {/* User Profile Image */}
-        <div className="absolute left-[50%] top-[5%] w-[230px] h-[230px] rounded-full z-20 transform -translate-x-[50%] overflow-hidden">
-            <Image src={ellipseImage} alt="DefaultPFP" layout="fill" objectFit="cover" className="rounded-full" />
+        <div className="absolute left-[50%] top-[5%] w-[230px] h-[230px]  z-20 transform -translate-x-[50%] overflow-hidden">
+            <Image src={ellipseImage} alt="DefaultPFP" layout="fill" objectFit="cover"  />
         </div>
     </div>
 
@@ -61,34 +69,135 @@ export default function ProfilePage() {
 
             {/* Conditionally render the team member info only if teamMember is defined */}
             {teamMember ? (
-                <>
-                <div className="absolute left-[25%] top-[45%] w-[191px] h-[26px] text-center text-black text-xl font-bold transform -translate-x-[50%]">
-            Your Information
-        </div>
-                    <div className="w-[101px] h-3.5 left-[99px] top-[397px] absolute text-black text-[15px] font-bold">First Name</div>
-                    <div className="w-[220px] h-7 left-[99px] top-[418px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.Fname}</div>
+            <>
+                {/* "Your Information" Label */}
+                <div className="absolute left-[25%] top-[42%] w-[191px] h-[26px] text-center text-black text-xl font-bold transform -translate-x-[50%]">
+                    Your Information
+                </div>
 
-                    <div className="w-[101px] h-3.5 left-[101px] top-[449px] absolute text-black text-[15px] font-bold">Last Name</div>
-                    <div className="w-[220px] h-7 left-[99px] top-[472px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.Lname}</div>
+                {/* First Name */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[360px] absolute text-black text-[13px] font-bold">First Name</div>
+                <div className="absolute left-[50px] top-[365px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={FirstNameImage} 
+                        alt="First Name Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[380px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.Fname}
+                </div>
 
-                    <div className="w-[125px] h-[19px] left-[99px] top-[660px] absolute text-black text-[15px] font-bold">Graduation Year</div>
-                    <div className="w-[241px] h-[22px] left-[100px] top-[680px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.GradYear}</div>
+                {/* Last Name */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[410px] absolute text-black text-[13px] font-bold">Last Name</div>
+                <div className="absolute left-[50px] top-[415px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={LastNameImage} 
+                        alt="Last Name Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[430px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.Lname}
+                </div>
 
-                    <div className="w-[101px] h-3.5 left-[101px] top-[719px] absolute text-black text-[15px] font-bold">Major</div>
-                    <div className="w-[241px] h-[22px] left-[101px] top-[740px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.Major}</div>
+                {/* Graduation Year */}
+                <div className="w-[125px] h-[15px] left-[99px] top-[460px] absolute text-black text-[13px] font-bold">Graduation Year</div>
+                <div className="absolute left-[50px] top-[465px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={GradYearImage} 
+                        alt="Graduation Year Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[480px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.GradYear}
+                </div>
 
-                    <div className="w-[125px] h-[19px] left-[99px] top-[505px] absolute text-black text-[15px] font-bold">Phone Number</div>
-                    <div className="w-[241px] h-[22px] left-[100px] top-[528px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.Phone}</div>
-            
-                    <div className="w-[101px] h-3.5 left-[101px] top-[557px] absolute text-black text-[15px] font-bold">E-mail</div>
-                    <div className="w-[241px] h-[22px] left-[101px] top-[578px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.Email}</div>
-            
-                    <div className="w-[101px] h-3.5 left-[100px] top-[609px] absolute text-black text-[15px] font-bold">CWID</div>
-                    <div className="w-[241px] h-[22px] left-[100px] top-[630px] absolute text-[#b9b9b9] text-[15px] font-bold">{teamMember.CWID}</div>
-                </>
-            ) : (
-                <div>No team member data available.</div>
-            )}
-        </div>
-    );
-}
+                {/* Major */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[510px] absolute text-black text-[13px] font-bold">Major</div>
+                <div className="absolute left-[50px] top-[515px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={MajorImage} 
+                        alt="Major Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[530px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.Major}
+                </div>
+
+                {/* Phone Number */}
+                <div className="w-[125px] h-[15px] left-[99px] top-[560px] absolute text-black text-[13px] font-bold">Phone Number</div>
+                <div className="absolute left-[50px] top-[565px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={PhoneNumberImage} 
+                        alt="Phone Number Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[580px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.Phone}
+                </div>
+
+                {/* Email */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[610px] absolute text-black text-[13px] font-bold">E-mail</div>
+                <div className="absolute left-[50px] top-[615px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={EmailImage} 
+                        alt="Email Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[630px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.Email}
+                </div>
+
+                {/* CWID */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[660px] absolute text-black text-[13px] font-bold">CWID</div>
+                <div className="absolute left-[50px] top-[665px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={CWIDImage} 
+                        alt="CWID Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[680px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.CWID}
+                </div>
+
+                {/* Status */}
+                <div className="w-[101px] h-[15px] left-[99px] top-[710px] absolute text-black text-[13px] font-bold">Status</div>
+                <div className="absolute left-[50px] top-[715px] w-[15px] h-[30px] z-20 transform -translate-x-[50%]">
+                    <Image 
+                        src={StatusImage} 
+                        alt="Status Icon" 
+                        layout="fill" 
+                        objectFit="contain"
+                        className="rounded"
+                    />
+                </div>
+                <div className="w-[220px] h-[20px] left-[99px] top-[730px] absolute text-[#b9b9b9] text-[13px] font-bold">
+                    {teamMember.CWID}
+                </div>
+            </>
+        ) : (
+            <div>No team member data available.</div>
+        )}
+    </div>
+);
+        }
