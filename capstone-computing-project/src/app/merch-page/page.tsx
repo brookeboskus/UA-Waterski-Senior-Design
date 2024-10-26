35c67f52c0ddc92340408a6f763cffe255010980
brian419 <json10@crimson.ua.edu>
2024-10-26 05:50:19 -0500

    merch page images are also not clickable. kept link element for merchandise so they can point to external source but added pointer-events-none so they're not clickable for now. before, it took us back to homepage

--- capstone-computing-project/src/app/merch-page/page.tsx
+++ capstone-computing-project/src/app/merch-page/page.tsx
@@ -38,7 +38,7 @@ export default function MerchPage() {
                     console.log(sheetData);
 
                     const image2AUrl = data[1][0];
-                    if (image2AUrl?.startsWith("http")){
+                    if (image2AUrl?.startsWith("http")) {
                         setImage2A(image2AUrl);
                     } else {
                         setImage2A(null);
@@ -58,39 +58,36 @@ export default function MerchPage() {
         getData();
     }, []);
 
-    
+
 
     return (
-        <div className="min-h-screen flex flex-col">
-            
-            <Link href = "/">
-                {image2A && (
-                    <img 
-                        src = {image2A}
-                        alt = "Image from 2A" 
-                        className = "h-200px w-200px md:h-250px md:w-250px lg:h-300px lg:w-300px max-w-full object-contain mr-20"
-                    />
-                )}
-            </Link>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+            {image2A && (
+                <img
+                    src={image2A}
+                    alt="Image from 2A"
+                    className="h-200px w-200px md:h-250px md:w-250px lg:h-300px lg:w-300px max-w-full object-contain"
+                />
+            )}
 
             <h2 className="text-2xl font-bold text-center mt-10" style={{ color: '#9E1B32' }}>Alabama Waterski Team Merchandise</h2>
 
             {/* Section for smaller images */}
             <div className="flex flex-wrap justify-center space-x-20 mt-10">
                 {!error && sheetData && sheetData.length > 0 && sheetData.slice(0).map((imageUrl, index) => (
-                    <Link key={index} href="/" className="flex flex-col flex-shrink-0 w-1/4">
-                        
-                        <img 
-                            src={imageUrl} 
-                            alt={`Image ${index + 2}`} 
-                            className="h-full w-auto object-contain" // Adjust sizes as needed
+                    <Link key={index} href="#" className="flex flex-col flex-shrink-0 w-1/4 pointer-events-none"> {/* added pointer-events-none for now disabling clicking images. currently takes to homepage, but taking link element requires href. can add external link later in href */}
+
+                        <img
+                            src={imageUrl}
+                            alt={`Image ${index + 2}`}
+                            className="h-full w-auto object-contain" 
                         />
-                    <div className="flex flex-col">
-                        <p className="mt-2 text-md text-black">{merchLabels [index + 1]}</p> {/* Label under each image */}
-                        <p className="mt-2 mb-20 text-md text-black">{merchPrices [index + 1]}</p> {/* Label under each image */}
-                    </div>
+                        <div className="flex flex-col">
+                            <p className="mt-2 text-md text-black">{merchLabels[index + 1]}</p> {/* Label under each image */}
+                            <p className="mt-2 mb-20 text-md text-black">{merchPrices[index + 1]}</p> {/* Label under each image */}
+                        </div>
                     </Link>
-                    
+
                 ))}
             </div>
 
