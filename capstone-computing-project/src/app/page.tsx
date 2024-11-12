b1a83b9a2a4213d5a67039c2f2544ea95f8dad4a
brian419 <json10@crimson.ua.edu>
2024-11-12 17:13:18 -0600

    adding z-10 to increase z index of buttons to be higher than images

--- capstone-computing-project/src/app/page.tsx
+++ capstone-computing-project/src/app/page.tsx
@@ -174,7 +174,7 @@ export default function Home() {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24"
                                 fill="currentColor"
-                                className="w-6 h-6 md:w-8 md:h-8"
+                                className="w-6 h-6 md:w-8 md:h-8 z-10"
                             >
                                 <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6z" />
                             </svg>
@@ -191,7 +191,7 @@ export default function Home() {
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24"
                                 fill="currentColor"
-                                className="w-6 h-6 md:w-8 md:h-8"
+                                className="w-6 h-6 md:w-8 md:h-8 z-10"
                             >
                                 <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6z" />
                             </svg>
