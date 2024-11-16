5b725281af673eda2de7f2f0b6b7a6779d09b636
brian419 <json10@crimson.ua.edu>
2024-11-16 08:58:14 -0600

    attempting to improve safari svg experience

--- capstone-computing-project/tailwind.config.ts
+++ capstone-computing-project/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.svg",
   ],
   theme: {
     extend: {
@@ -19,6 +21,22 @@ const config: Config = {
       },
     },
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".image-rendering-crisp": {
+          "image-rendering": "crisp-edges",
+        },
+        ".image-rendering-pixel": {
+          "image-rendering": "pixelated",
+        },
+        ".image-rendering-optimize": {
+          "image-rendering": "-webkit-optimize-contrast",
+        },
+      };
+
+      addUtilities(newUtilities);
+    }),
+  ],
 };
 export default config;
