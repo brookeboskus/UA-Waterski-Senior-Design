20e639cfe3848bd60b82f79b10becbaa0cf9541e
brian419 <json10@crimson.ua.edu>
2024-11-16 09:04:43 -0600

    reverting tailwind.config.ts changes and attempting to change svg instead

--- capstone-computing-project/tailwind.config.ts
+++ capstone-computing-project/tailwind.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.svg",
   ],
   theme: {
     extend: {
@@ -14,29 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      imageRendering: {
-        crisp: 'crisp-edges',
-        pixel: 'pixelated',
-        optimize: '-webkit-optimize-contrast', 
-      },
     },
   },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      const newUtilities = {
-        ".image-rendering-crisp": {
-          "image-rendering": "crisp-edges",
-        },
-        ".image-rendering-pixel": {
-          "image-rendering": "pixelated",
-        },
-        ".image-rendering-optimize": {
-          "image-rendering": "-webkit-optimize-contrast",
-        },
-      };
-
-      addUtilities(newUtilities);
-    }),
-  ],
+  plugins: [],
+  
 };
 export default config;
