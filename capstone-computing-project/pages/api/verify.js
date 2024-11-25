40a902b59c395c7c0ae008facccd1ceca444c4f1
brian419 <json10@crimson.ua.edu>
2024-11-25 02:37:06 -0600

    checking if user is already verified

--- capstone-computing-project/pages/api/verify.js
+++ capstone-computing-project/pages/api/verify.js
@@ -38,17 +38,23 @@ let APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 
 const verify = async (req, res) => {
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     const { token } = req.query;
-    console.log("token in verify.js:", token);
+    console.log("Token in verify.js:", token);
 
     try {
         const [user] = await db.query('SELECT * FROM User WHERE VerificationToken = ?', [token]);
         console.log("Query result:", user);
 
         if (!user || user.length === 0) {
-            console.log("Redirecting to failed page...");
+            console.log("Invalid or expired token. Redirecting to failed page...");
             const endpoint = `${APP_URL}verification-failed-page`;
+            res.redirect(endpoint);
+            return;
+        }
+
+        if (user[0].IsVerified) {
+            console.log("User already verified. Redirecting to success page...");
+            const endpoint = `${APP_URL}verification-success-page`;
             res.redirect(endpoint);
             return;
         }
@@ -59,7 +65,7 @@ const verify = async (req, res) => {
             [true, token]
         );
         console.log("Database updated successfully. Redirecting to success page...");
-        
+
         const endpoint = `${APP_URL}verification-success-page`;
         res.redirect(endpoint);
     } catch (error) {
@@ -67,6 +73,9 @@ const verify = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
+
+export default verify;
+
 
 
 export default verify;
